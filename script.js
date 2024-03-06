5eb6653d78db7778e534935a1ebbfe8256f653d7
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-03-06 21:39:04 +0700

    add about and education

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const buttonsData = [
-  { imageUrl: "./img/map.svg", linkUrl: "mailto:aufaikrimah@gmail.com" },
-  { imageUrl: "./img/map.svg", linkUrl: "whatsapp://send?phone=6289506591035" },
-  { imageUrl: "./img/map.svg", linkUrl: "https://github.com/aufaikrimaa" },
+  { imageUrl: "./img/email.svg", linkUrl: "mailto:aufaikrimah@gmail.com" },
+  { imageUrl: "./img/wa.svg", linkUrl: "whatsapp://send?phone=6289506591035" },
+  { imageUrl: "./img/github.svg", linkUrl: "https://github.com/aufaikrimaa" },
   {
-    imageUrl: "./img/map.svg",
+    imageUrl: "./img/linkedin.svg",
     linkUrl: "https://www.linkedin.com/in/aufa-ikrimah/",
   },
 ];
@@ -14,7 +14,7 @@ buttonsData.forEach((data) => {
   const buttonHTML = `
           <a href="${data.linkUrl}">
               <span>
-                  <img src="${data.imageUrl}" alt="Button Image" height="25">
+                  <img src="${data.imageUrl}" alt="Button Image" height="20">
               </span>
           </a>
       `;
